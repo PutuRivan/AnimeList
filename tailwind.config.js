93a009c914b659fc3df1fc0e2749be05ecd08464
PutuRivan <150578221+PutuRivan@users.noreply.github.com>
2024-06-14 23:52:05 +0700

    fix:remove color daisy

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,34 +15,6 @@ module.exports = {
         'Jet' : '#333533'
       }
     },
-    daisyui: {
-      themes: [
-        {
-          mytheme: {
-            "primary": "#0088ff",
-            "primary-content": "#000616",
-            "secondary": "#915100",
-            "secondary-content": "#eadbcf",
-            "accent": "#0012ff",
-            "accent-content": "#c7dcff",
-            "neutral": "#191511",
-            "neutral-content": "#cbcac9",
-            "base-100": "#25292e",
-            "base-200": "#1f2227",
-            "base-300": "#191c1f",
-            "base-content": "#ced0d1",
-            "info": "#009cd7",
-            "info-content": "#000911",
-            "success": "#00ffbf",
-            "success-content": "#00160d",
-            "warning": "#c05c00",
-            "warning-content": "#0e0300",
-            "error": "#cc002e",
-            "error-content": "#fcd6d4",
-          },
-        },
-      ],
-    },
   },
   plugins: [require("daisyui")],
 };
